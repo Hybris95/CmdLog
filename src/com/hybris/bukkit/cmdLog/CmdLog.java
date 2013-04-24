a45d845914ed081eebebaf43ee962a8888f26836
Hybris95 <hybris_95@hotmail.com>
2013-04-24 07:50:19 -0400

    Added my mail address into the Licence

--- src/com/hybris/bukkit/cmdLog/CmdLog.java
+++ src/com/hybris/bukkit/cmdLog/CmdLog.java
@@ -1,6 +1,7 @@
 /*
     CmdLog - The Command Logger plugin for CraftBukkit
     Copyright (C) 2013  Hybris95
+    hybris_95@hotmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
